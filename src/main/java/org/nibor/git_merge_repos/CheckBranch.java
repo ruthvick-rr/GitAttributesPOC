34c5c86003a55b1877d28688399d961563ec93df
Ruthvick <ruthvick.ravu@gmail.com>
2025-10-01 20:18:09 -0500

    test all

--- src/main/java/org/nibor/git_merge_repos/CheckBranch.java
+++ src/main/java/org/nibor/git_merge_repos/CheckBranch.java
@@ -1,4 +1,5 @@
-public class ruthvick {
+public class CheckBranch
+ {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("ruthvick");
